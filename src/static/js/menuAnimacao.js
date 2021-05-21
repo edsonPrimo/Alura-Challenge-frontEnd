f92574e36df01731fc6d1f65324a7a8634ea1543
Edson Primo <edson.oliveira00@hotmail.com>
2021-05-21 11:43:54 -0300

    menu hamburguer

---
+++ src/static/js/menuAnimacao.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector("[data-icone]")
+const menu = document.querySelector("[data-hamburguer]")
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu__open')
+    btnMenu.classList.toggle('overflow')
+})
